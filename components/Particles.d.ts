3d3e3ecb39acafe5c2a1455db1908ca65da4a15f
unemployedz <dhaitifrancky4@gmail.com>
2026-09-13 13:22:46 -0400

    Fix Particles TypeScript declaration

--- components/Particles.d.ts
+++ components/Particles.d.ts
@@ -1,21 +1,20 @@
-declare module "./Particles" {
-  import * as React from "react";
+import * as React from "react";
 
-  type ParticlesProps = {
-    particleCount?: number;
-    particleSpread?: number;
-    speed?: number;
-    particleColors?: string[];
-    moveParticlesOnHover?: boolean;
-    particleHoverFactor?: number;
-    alphaParticles?: boolean;
-    particleBaseSize?: number;
-    sizeRandomness?: number;
-    cameraDistance?: number;
-    disableRotation?: boolean;
-    pixelRatio?: number;
-  };
+type ParticlesProps = {
+  particleCount?: number;
+  particleSpread?: number;
+  speed?: number;
+  particleColors?: string[];
+  moveParticlesOnHover?: boolean;
+  particleHoverFactor?: number;
+  alphaParticles?: boolean;
+  particleBaseSize?: number;
+  sizeRandomness?: number;
+  cameraDistance?: number;
+  disableRotation?: boolean;
+  pixelRatio?: number;
+};
 
-  const Particles: React.FC<ParticlesProps>;
-  export default Particles;
-}
+declare const Particles: React.FC<ParticlesProps>;
+
+export default Particles;
